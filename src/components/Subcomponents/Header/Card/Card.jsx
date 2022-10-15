bdbf122ad3020e2b94fc4d60ac676212dc7c13ff
Maggie-Sarmiento <gsmaggie001@gmail.com>
2022-10-15 02:38:08 -0500

    create-modaldetails for each product

--- src/components/Subcomponents/Header/Card/Card.jsx
+++ src/components/Subcomponents/Header/Card/Card.jsx
@@ -5,9 +5,11 @@ import {
   ThemeProvider,
   Typography,
 } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import styles from './Card.module.css'
+import ModalDetails from './ModalDetails/ModalDetails';
+import { Container } from '@mui/system';
 
 const threeMenuStyle = {
   display: 'flex',
@@ -53,64 +55,80 @@ const theme = createTheme({
   },
 });
 
-const Card = ({id, 
-  product
-}) => (
-  <Grid item xs={6} sm={3}>
-    <ThemeProvider theme={theme}>
-      <Paper elevation={3} className="paper">
-        <img src={product.img} alt="" className={styles.imgCard} />
-        <Box
-          sx={{
-            paddingX: 1,
-            position: 'relative',
-          }}
-        >
-          <Box>
+const Card = ({ id, product }) => {
+
+
+  const [open, setOpen] = useState(false)
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false)
+
+  return (
+    <>
+      <Grid item xs={12} sm={6} md={3} >
+        <ThemeProvider theme={theme}>
+          <Paper elevation={3} className="paper" onClick={handleOpen}>
+            <img src={product.img} alt="" className={styles.imgCard} />
             <Box
               sx={{
-                display: 'flex',
-                alignItems: 'center',
+                paddingX: 2,
+                position: 'relative',
               }}
-              marginTop={0.5}
-            >
-            </Box>
-          </Box>
-          <Typography
-            variant="subtitle1"
-            component="h2"
-            className={styles.productName}
-            sx={{
-              alignSelf: 'center',
-              fontWeight: 'bold',
-              fontSize: '0.8em',
-            }}
-          >
-            {product.title}
-          </Typography>
+              >
+              <Box>
+                <Box
+                  sx={{
+                    display: 'flex',
+                    alignItems: 'center',
+                  }}
+                  marginTop={0.5}
+                  >
+                </Box>
+              </Box>
+              <Typography
+                variant="subtitle1"
+                component="h2"
+                className={styles.productName}
+                sx={{
+                  alignSelf: 'center',
+                  fontWeight: 'bold',
+                  fontSize: '0.8em',
+                }}
+                height={40}
+                >
+                {product.title}
+              </Typography>
 
-          <Box
-            sx={{
-              display: 'flex',
-            }}
-          >
-            <Typography
-              variant="h6"
-              component="h3"
-              className={styles.productPrice}
-              sx={{
-                fontSize: '0.8em',
-              }}
-            >
-              $&nbsp;
-              {product.price}
-              &nbsp;MXN
-            </Typography>
-          </Box>
-        </Box>
-      </Paper>
-    </ThemeProvider>
-  </Grid>
-);
+              <Box
+                sx={{
+                  display: 'flex',
+                }}
+                marginTop={0.8}
+                >
+                <Typography
+                  variant="h6"
+                  component="h3"
+                  className={styles.productPrice}
+                  sx={{
+                    fontSize: '0.8em',
+                  }}
+                  >
+                  $&nbsp;
+                  {product.price}
+                  &nbsp;MXN
+                </Typography>
+              </Box>
+            </Box>
+          </Paper>
+        </ThemeProvider>
+      </Grid>
+      <ModalDetails 
+        id={id}
+        product={product}
+        open={open}
+        setOpen={setOpen} 
+      />
+    </>
+  )
+};
 
 export default Card;
